59ef37e5a0f272f7a6f131abf2803412c149258d
Leo <77863733+leofox2021@users.noreply.github.com>
2023-07-08 04:29:27 +0400

    add duplicates cleaning to repository cleaner

--- Services/RepositoryCleaner.cs
+++ Services/RepositoryCleaner.cs
@@ -8,7 +8,20 @@ public static class RepositoryCleaner
 {
     public static void Clean(BaseRepository<ExtendedWeatherRecord> repository)
     {
+        // Remove not found cities
         foreach (var weatherRecord in repository.GetAll().Where(n => n.City.Equals(StringConstants.CityNotExist)))
             repository.Delete(weatherRecord.Id);
+
+        // Remove duplicates 
+        foreach (var weatherRecord in repository.GetAll())
+        {
+            var reportsWithSameCity = repository.GetAll().Where(n => n.City.Equals(weatherRecord.City)).ToList();
+            
+            if (reportsWithSameCity.Count == 1 || !reportsWithSameCity.Any()) 
+                return;
+            
+            for (int i = 1; i < reportsWithSameCity.Count(); i++)
+                repository.Delete(reportsWithSameCity.ElementAt(i).Id);
+        }
     }
 }
